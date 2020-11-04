e30a65aa663451d3ae8a1e68ff06c2d9de9a45f7
TypMitSchnurrbart <mueller.alexan-it19@it.dhbw-ravensburg.de>
2020-11-04 12:41:04 +0100

    Second Test Commit

---
+++ scripts/test_main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+#!-*- coding: utf-8 -*-
+
+print("Content-Type: text/html")
+print("")
+print("""
+<html>
+<head>
+    <title>DACH Group Inc.</title>
+</head>
+<body>
+    <h1>Jausa, lüppt!</h1>
+</body>
+</html>
+""")
