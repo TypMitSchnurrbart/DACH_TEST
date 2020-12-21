d7194daef8879aecb9e2d73c81a28cde29d8fd0a
TypMitSchnurrbart <61209252+TypMitSchnurrbart@users.noreply.github.com>
2020-12-21 10:03:36 +0100

    Looking into SQL Insert Error

--- scripts/files/login.py
+++ scripts/files/login.py
@@ -9,17 +9,11 @@ def register_user(data_array, data_handle):
     return: {bool}  True if a Error occurs, else False
     """
 
-    #TODO Check if data_array contains valid data; especially password and unique password!
+    #TODO Check if data_array contains valid data; especially password = repeat_pw and unique email!
 
-    try:
-        #TODO Change the TEST BOOL to 0 before rollout
-        data_handle.execute(f"""INSERT INTO user (vorname, nachname, strasse, hausnr, plz, ort, email, password, TEST) VALUES ({data_array[0][1]}, {data_array[1][1]}, {data_array[2][1]}, {data_array[3][1]}, {data_array[4][1]}, {data_array[5][1]}, {data_array[6][1]}, {data_array[7][1]}, 1)""")
-
-        data_handle.commit()
-
-    except:
-        print(f"Error in SQL Insert!")
-        return True
+    
+    data_handle.execute(f"""INSERT INTO user (vorname, nachname, strasse, hausnr, plz, ort, email, password) VALUES ({data_array[0][1]}, {data_array[1][1]}, {data_array[2][1]}, {data_array[3][1]}, {data_array[4][1]}, {data_array[5][1]}, {data_array[6][1]}, {data_array[7][1]})""")
+    data_handle.commit()
 
     return False
 
