55392a6043f82a27991aad93923156a2cdbb5408
Dirk Hattemer <dirk.hattemer@gmail.com>
2021-01-14 12:16:47 +0100

    dashboard-jsson.py

--- scripts/files/dashboard-json.py
+++ scripts/files/dashboard-json.py
@@ -17,21 +17,28 @@ def show_homepage(data_array):
     #Like this only for test; Ident should be the email but somehow hashed
     ident_value = data_array[0][1]
 
-output = f"""{
-    'ident_value': '{ident_value}',
-    "status": "{covid_state}",
-    "room": [
-        "room1": "{room1}",
-        "room2": "{room2}",
-        "room3": "{room3}",
-        "room4": "{room4}",
-    ],
-    "date": "{date}"
-}
-"""
+    output = f"""
+        (
+            "ident_value": "{ident_value}",
+            "state": "{state}",
+            "lastRoom": "{lastRoom}",
+            "activeUser": "{activeUser}",
+            "lastUpdate": "{lastUpdate}",
+            "version": "{version}",
+            "roomHistory": [
+                ( "room": "{room1}", "date": "{date1}", "time": "{time1}", "nrUser": "{nrUser1}"),
+                ( "room": "{room2}", "date": "{date2}", "time": "{time2}", "nrUser": "{nrUser2}"),
+                ( "room": "{room3}", "date": "{date3}", "time": "{time3}", "nrUser": "{nrUser3}"),
+                ( "room": "{room4}", "date": "{date4}", "time": "{time4}", "nrUser": "{nrUser4}"),
+                ( "room": "{room5}", "date": "{date5}", "time": "{time5}", "nrUser": "{nrUser5}")
+            ]
+        )
+    """
+    output = output.replace("(", "{")
+    output = output.replace(")", "}")
 
-print(output)
+    print(output)
 
-#TODO Delete MOVE_Test from sidenav
-#TODO Hash the ident; store in extra db table; not to pretty but should be alright
-return
+    #TODO Delete MOVE_Test from sidenav
+    #TODO Hash the ident; store in extra db table; not to pretty but should be alright
+    return
