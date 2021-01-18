81050b30893cd39b15c2c67067ad4e20fcf0ab6e
Timelim17 <fassbender.tim-it19@it.dhbw-ravensburg.de>
2021-01-18 15:13:43 +0100

    testing

--- scripts/files/home_page.py
+++ scripts/files/home_page.py
@@ -140,7 +140,7 @@ def show_homepage(data_array):
     print(output)
 
     #Get the table done
-    if (last_rooms_array[0] == "$false$"):
+    if (last_rooms_array[0] == "Empty"):
         print(f"""                          <tr>
                                                  <td>Kein Raum in den letzten 14 Tagen!</td>
                                                  <td></td>
